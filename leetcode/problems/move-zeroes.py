1f57a1499b4ae98eee8a94496d3078700a5f35a9
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-01-07 15:49:49 +0200

    added more problems

---
+++ leetcode/problems/move-zeroes.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        lastNonZeroFoundAt = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[lastNonZeroFoundAt] = nums[i]
+                lastNonZeroFoundAt = lastNonZeroFoundAt + 1
+        
+        for i in range(lastNonZeroFoundAt, len(nums)):
+            nums[i] = 0
